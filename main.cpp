ac915519a538d2403ff1652af8cdee4c5f2ccbb6
Polinok <polinok.apalina2012@yandex.ru>
2024-06-27 01:56:39 +0300

    added animal iface

--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@ int main()
 {
     Printing::Printer printer;
 
-    printer.print(" Hello Word! ");
-    std::cerr << " принтеров создано: " << Printing::Printer::getCountCreatedPrinters() << std::endl;
+    printer.print("Hello Word!");
+    std::cerr << "Printerov sozdano: " << Printing::Printer::getCountCreatedPrinters() << std::endl;
 
     Printing::Printer* ptrToprinter = nullptr;
 
     ptrToprinter = new Printing::Printer();
 
-    std::cerr << " принтеров создано: " << Printing::Printer::getCountCreatedPrinters() << std::endl;
+    std::cerr << "Printerov sozdano: " << Printing::Printer::getCountCreatedPrinters() << std::endl;
 
     delete ptrToprinter;
 
-    std::cerr << " принтеров создано: " << Printing::Printer::getCountCreatedPrinters() << std::endl;
+    std::cerr << "Printerov sozdano: " << Printing::Printer::getCountCreatedPrinters() << std::endl;
 
 	return 0;
 }
