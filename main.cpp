371e65c4c77c2bbe60c190cbdd48ea39b84e68a7
Polinok <polinok.apalina2012@yandex.ru>
2024-10-02 08:38:58 +0300

    changed main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include "printer/printer.h"
+#include "complex_0110/complex_0110.h"
+#include "C:\Users\ROG\Documents\TESTS\TDDProject\TestTask\libs\complex_0110\include\complex_0110\complex_0110.h"
+#include <iostream>
+#include <math.h>
 
 int main()
 {
+
+    /*
     Printing::Printer printer;
 
     printer.print("Hello Word!");
@@ -16,6 +22,20 @@ int main()
     delete ptrToprinter;
 
     std::cerr << "Printerov sozdano: " << Printing::Printer::getCountCreatedPrinters() << std::endl;
+*/
+    Complex_0110 T1;
+
+    T1.view();
+
+    Complex_0110 T2(6);
+
+    T2.view();
+
+    Complex_0110 T3(2, 6);
+
+    T3.view();
+
+
 
 	return 0;
 }
