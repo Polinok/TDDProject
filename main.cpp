0fa5fa18784eae7107192c387a1fc664f1fc551a
Polinok <polinok.apalina2012@yandex.ru>
2024-10-19 00:02:01 +0300

    added wav file creation (not work)

--- main.cpp
+++ main.cpp
@@ -6,10 +6,27 @@
 #include <iterator>
 #include <algorithm>
 #include <math.h>
-#include <cstdio>
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
 
-#include <cstdlib>
+struct WavHead{
+ char RIFF[4];    //The RIFF in the first part
+ long int size0;//Save the size of all files behind
+ char WAVE[4];
+ char FMT[4];
+ long int size1;//Stored is the size saved by fmt, after including this, the first few data, a total of 16
+ short int fmttag;
+ short int channel;
+ long int samplespersec;//The number of samples per second, using 11025
+ long int bytepersec;
+ short int blockalign;
+ short int bitpersamples;
+ char DATA[4];
+ long int size2;//The remaining file size, that is, the size of the sound sample, because it is one second of content, then it is 11025.
+};
+
 using namespace std;
 int main()
 {
@@ -65,9 +82,9 @@ int main()
     std::cout << "  T9 = Complex(2,-4) / Complex(-1,2) = " << T9;
 
 
-    std::ifstream file;
-    file.open("C:\\Users\\ROG\\Documents\\TESTS\\TDDProject\\am_sound.dat", std::ios::binary);
-    if (file.is_open())
+    std::ifstream infile;
+    infile.open("C:\\Users\\ROG\\Documents\\TESTS\\TDDProject\\am_sound.dat", std::ios::binary);
+    if (infile.is_open())
         std::cout << "\n  File is opened\n" << std::endl;
     else
     {
@@ -76,15 +93,15 @@ int main()
     }
 
     int weight = 0;
-    file.seekg (0, std::ios::end); // становимся в конец файла || std::ios::end или std::ios_base::end
-    weight = file.tellg(); // считываем текущую позицию
-    file.seekg(0, std::ios::beg); // возвращаемся в начало
+    infile.seekg (0, std::ios::end); // становимся в конец файла || std::ios::end или std::ios_base::end
+    weight = infile.tellg(); // считываем текущую позицию
+    infile.seekg(0, std::ios::beg); // возвращаемся в начало
     std::cout << "  File size = " << weight << " B" << std::endl << std::endl;
     int size = weight / sizeof(int); // (количество элементов) = (общий вес) / (вес одного элемента типа int)
     std::cout << "  File contains " << size << " elements" << std::endl << std::endl;
 
     std::vector<Complex<int>> signalAM(size);
-    file.read((char*) &signalAM[0], weight);
+    infile.read((char*) &signalAM[0], weight);
     std::cout << "  X = " << signalAM[1000] << std::endl;
     std::vector<double> demodAM(size);
     for (int i = 0; i < size; i++) {
@@ -92,7 +109,17 @@ int main()
     }
     std::cout << " |X| = " << demodAM[1000] << std::endl;
 
-    file.close();
+    infile.close();
+
+    std::ofstream outfile("output.wav", std::ios::binary);
+    WavHead head={{'R','I','F','F'},0,{'W','A','V','E'},{'f','m','t',' '},16,
+                1,1,11025,11025 * sizeof(double),sizeof(double),8,{'d','a','t','a'},
+                0};
+
+    head.size2 = size * sizeof(double);
+    head.size0 = 36 + head.size2;
+    outfile.write((char*) &head, sizeof(head));
+    outfile.write((char*) &demodAM, sizeof(demodAM));
 
 	return 0;
 }
