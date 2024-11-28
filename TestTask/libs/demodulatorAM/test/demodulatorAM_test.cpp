8a0d2a3bb7022147ffb3ae9473a44a0257562646
Polinok <polinok.apalina2012@yandex.ru>
2024-11-28 21:26:09 +0300

    added tests for DemodulatorAM and DemodulatorFM

--- TestTask/libs/demodulatorAM/test/demodulatorAM_test.cpp
+++ TestTask/libs/demodulatorAM/test/demodulatorAM_test.cpp
@@ -1,5 +1,17 @@
 #include <gtest/gtest.h>
 #include "demodulatorAM/demodulatorAM.h"
 
+TEST(DemodulatorAMTests, answertest) {
+    std::vector<Complex<int>> signalinput(5);
+    std::vector<double> signalanswer = {0, 1, 2, 3, 4};
+    for (int i = 0;  i < 5; i++) {
+        signalinput[i] = static_cast<int>(i);
+    }
 
+    Demodulator<int>* DMAM = new DemodulatorAM<int>();
+    DMAM->demod(signalinput.data(), 5);
 
+    auto signaloutput = DMAM->getDemodData(); // получение демодулированного сигнала
+
+    ASSERT_EQ(signaloutput.data, signalanswer);
+}
--- TestTask/libs/demodulatorFM/test/demodulatorFM_test.cpp
+++ TestTask/libs/demodulatorFM/test/demodulatorFM_test.cpp
@@ -1,5 +1,20 @@
 #include <gtest/gtest.h>
 #include "demodulatorFM/demodulatorFM.h"
 
+TEST(DemodulatorFMTests, answertest) {
+    std::vector<Complex<int>> signalinput(5);
+    std::vector<double> signalanswer = {0, -0.8, -0.1, 1, 0.3};
+    signalinput[0] = {1, 3};
+    signalinput[1] = {3, 1};
+    signalinput[2] = {10, 0};
+    signalinput[3] = {3, 1};
+    signalinput[4] = {0, 10};
 
+    Demodulator<int>* DMFM = new DemodulatorFM<int>();
+    DMFM->demod(signalinput.data(), 5);
+
+    auto signaloutput = DMFM->getDemodData(); // получение демодулированного сигнала
+
+    ASSERT_EQ(signaloutput.data, signalanswer);
+}
 
